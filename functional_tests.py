da2d2a9377888730ef978f9ae98268f198554351
kirktrekkie <kirk@mail2trekkie.com>
2015-11-11 16:31:00 +0100

    More on functional_test.py, updated home.html with item in table, views.py can POST and more unit tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -37,19 +38,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers',  [row.text for row in rows])
 
         #There is still a box inviting her to add another item
         #She enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         #The page updates again and show both her items
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1. By peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
         #Edit wonder whether the list will remeber her list. Then she sees
         #that the site has generated a unique URL for here -- there is some
         #explanatory text to that effect
+        self.fail('Finish the test!')
 
         #She visits that URL - here to-do list is still there
 
