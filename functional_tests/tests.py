e0b91a6f39c26e4821202fef01f75468fe7c5bbc
kirktrekkie <kirk@mail2trekkie.com>
2015-11-16 18:08:08 +0100

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -41,7 +41,7 @@ class NewVisitorTest(LiveServerTestCase):
         #and not the page lists "1: Buy peacock feathers" as an item in a #to-do list table
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url,'/list/.+')
+        self.assertRegex(edith_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #There is still a box inviting her to add another item
